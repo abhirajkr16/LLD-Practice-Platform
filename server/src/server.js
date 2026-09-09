5f6fc16150dc32f1f29720772bbdbf0a8f14bee8
Abhiraj kumar <abhirajmait16@gmail.com>
2026-09-09 14:24:48 +0530

    feat: initialize project structure

---
+++ server/src/server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+
+
+
+
+
+
+app.get("/api/health", (req, res) => {
+    res.json({ message: "OK" })
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+})
